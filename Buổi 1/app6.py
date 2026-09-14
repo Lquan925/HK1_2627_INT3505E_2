9ffc7363fe7865c8266b43456ef1c38f05cde533
Lquan925 <hongquanle596@gmail.com>
2026-09-14 23:14:57 +0700

    update:readme

--- Buổi 1/app6.py
+++ Buổi 1/app6.py
@@ -4,6 +4,9 @@ BOOKS = [
     {"id": 1, "title" : "book 1", "author": "author 1"},
     {"id": 2, "title" : "book 2", "author": "author 2"},
     {"id": 3, "title" : "book 3", "author": "author 3"},
+    {"id": 4, "title" : "book 4", "author": "author 4"},
+    {"id": 5, "title" : "book 5", "author": "author 5"},
+    {"id": 6, "title" : "book 6", "author": "author 6"}
 ]
 _next = len(BOOKS) + 1
 def find(bid):
